5ecbd68cb79e87ac4b24e891dd80eb3fdee2d5f8
XiangyuDu <hnyd1995@163.com>
2019-01-02 17:02:11 +0800

    add websocket

--- src/network/utils.js
+++ src/network/utils.js
@@ -2,7 +2,30 @@
  * Created by Captain on 2018/5/16 9:51.
  */
 
-let util = {
+// --- WebSocket ---
+// let url = 'ws://127.0.0.1:8888/websocket';
+let url = 'ws://10.206.199.254:3000/websocket';
+let ws = new WebSocket(url);
+
+ws.onopen = function (event) {
+  console.log('===> [websocket] open');
+  ws.onmessage = function (message) {
+    console.log('===> [websocket] message: ', message);
+    // TODO
+  };
+
+  ws.onerror = function (error) {
+    console.log('===> [websocket] error: ', error);
+    // TODO
+  };
+
+  ws.onclose = function () {
+    console.log('===> [websocket] close');
+    // TODO
+  }
+};
+
+let network = {
 
   axios: null,
 
@@ -20,5 +43,13 @@ let util = {
 
 };
 
-export default util;
+// websocket 测试发送
+// let body = {
+//   type: 'upload',
+//   data: {},
+//   desc: 'websocket body desc'
+// };
+// ws.send(JSON.stringify(body));
+
+export {network, ws};
 // module.exports = util;
